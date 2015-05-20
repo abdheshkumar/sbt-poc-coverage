fe965626565b7929dfaf3b04c748b490d64d1b27
abdhesh <abdheshkumar90.rkg@gmail.com>
2015-05-20 14:38:50 +0530

    add script file

---
+++ src/test/resources/schemadata.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user` values (1,'abdhesh kumar','abdhesh.kumar@searsglobaltech.com'), (2,'piyush','piyush@searsglobaltech.com'),
+(3,'Rohan','rohan@mvtechlab.com'),(4,'Santosh','santosh@mvtechlabs.com'),(5,'Bharat','bharat@mvtechlabs.com');
+
+
